6372c4d9cab893a06728cab9e65a39f67f52a131
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-08-29 07:59:38 +0200

    update configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   content: ["./src/**/*.tsx", "./src/**/*.ts"],
   theme: {
     extend: {
-      
+      colors: {
+        mainBlue: "#019FFE",
+      },
     },
   },
   plugins: [],

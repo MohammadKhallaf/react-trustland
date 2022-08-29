dd307a99905197e0364ad6bda4d51dfdf4a0579b
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-08-29 08:00:29 +0200

    add header component

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,23 @@
 import React from "react";
-
+import logoSVG from "@/assets/images/TrustLogo.svg";
 const Header = (): JSX.Element => {
-  return <header>Header</header>;
+  return (
+    <header className="flex flex-row justify-around bg-black text-white py-3 align-baseline">
+      <div>
+        <img src={logoSVG} alt="trust logo" className="h-6" />
+      </div>
+      <ul className="flex flex-row gap-3">
+        <li>ترست لاين</li>
+        <li>لماذا ترست لاين؟</li>
+        <li>آلية العمل</li>
+        <li>تواصل معنا</li>
+      </ul>
+      <button className="rounded-full bg-white text-black px-3 py-1 text-sm">
+        دخول
+      </button>
+      <div>ENG</div>
+    </header>
+  );
 };
 
 export default Header;

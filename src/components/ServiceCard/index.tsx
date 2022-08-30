a0a03302195f2c3fc62349ebba3e234da1e45cf3
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2022-08-30 11:09:26 +0200

    update services section

--- src/components/ServiceCard/index.tsx
+++ src/components/ServiceCard/index.tsx
@@ -11,14 +11,15 @@ const ServiceCard = ({
   icon,
 }: ServiceCardProps): JSX.Element => {
   return (
-    <div className="flex flex-row items-center shadow-lg p-5 divide-mainBlue divide-x-2">
-      <div className="flex flex-col  justify-center items-center">
+    <div className="flex flex-row items-center shadow-lg p-5 rounded-2xl bg-lightGray">
+      <div className="flex flex-col justify-center items-center border-l-2 border-l-mainBlue pl-3">
         <div>
           <img src={icon} alt="" className="w-6" />
         </div>
-        <h5 className="font-boold">{title}</h5>
+        <h5 className="font-bold text-center text-mainBlue">{title}</h5>
       </div>
-      <div>{description}</div>
+
+      <div className="pr-3 text-darkGray">{description}</div>
     </div>
   );
 };
--- src/containers/ServicesSection/index.tsx
+++ src/containers/ServicesSection/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import ServiceCard from "../../components/ServiceCard";
 
-import redTeam from "@img/service-people.svg";
+import peopleIcon from "@img/solutions/people.svg";
+import shieldIcon from "@img/solutions/shield.svg";
+import trendingUpIcon from "@img/solutions/trending-up.svg";
+import codeIcon from "@img/solutions/code.svg";
+import keyboardIcon from "@img/solutions/keyboard-open.svg";
 
 const ServicesSection = (): JSX.Element => {
   return (
-    <section className="bg-black pt-5">
+    <section className="bg-black py-5">
       <div className="bg-white rounded-3xl container-spacing ">
-        <div className="p-5 grid grid-cols-2 grid-rows-3">
+        <div className="p-5 grid  md:grid-cols-2 grid-rows-3 gap-4">
           <div className="header__line">
             <h3 className="font-bold text-3xl">الخدمات والحلول</h3>
             <p>نقدم خدمات وحلول احترافية في مجال الأمن السيبراني</p>
@@ -16,7 +20,31 @@ const ServicesSection = (): JSX.Element => {
             title="الفريق الأحمر"
             description="مراجعة الكود المصدري للتطبيق في محاولة لتحديد وإزالة نقاط الضعف المتعلقة
         بالأمان"
-            icon={redTeam}
+            icon={peopleIcon}
+          />
+          <ServiceCard
+            title="تقييم الضعف"
+            description="مراجعة الكود المصدري للتطبيق في محاولة لتحديد وإزالة نقاط الضعف المتعلقة
+        بالأمان"
+            icon={trendingUpIcon}
+          />
+          <ServiceCard
+            title="المصادر الأمنية"
+            description="مراجعة الكود المصدري للتطبيق في محاولة لتحديد وإزالة نقاط الضعف المتعلقة
+        بالأمان"
+            icon={shieldIcon}
+          />
+          <ServiceCard
+            title="مراجعة كود المصدر"
+            description="مراجعة الكود المصدري للتطبيق في محاولة لتحديد وإزالة نقاط الضعف المتعلقة
+        بالأمان"
+            icon={codeIcon}
+          />
+          <ServiceCard
+            title="فحص الموقع والجوال"
+            description="مراجعة الكود المصدري للتطبيق في محاولة لتحديد وإزالة نقاط الضعف المتعلقة
+        بالأمان"
+            icon={keyboardIcon}
           />
         </div>
       </div>

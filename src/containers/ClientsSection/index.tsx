cecf63893726253f6421aa6305cffeb333d1cd1c
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-08-31 19:30:24 +0200

    add clients section

--- src/containers/ClientsSection/index.tsx
+++ src/containers/ClientsSection/index.tsx
@@ -1,37 +1,67 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import { Navigation } from "swiper";
+import styled from "styled-components/macro";
 import SectionHeader from "../../components/SectionHeader";
 
 import ZidLogo from "@img/clients/zid-logo.png";
 import MarsadLogo from "@img/clients/marsad-logo.png";
 import MonshaatLogo from "@img/clients/monshaat-logo.png";
 
-import "swiper/css";
+import prevIcon from "@img/carousel/arrow-right.svg";
+import nextIcon from "@img/carousel/arrow-left.svg";
 import "swiper/css/navigation";
+
+const NavIcon = styled.img`
+  height: 100px;
+  &:hover {
+  }
+`;
 
 const ClientsSection = (): JSX.Element => {
   return (
-    <section className="bg-blue-400 px-5 py-16">
+    <section className=" px-5 py-16 container-spacing">
       <SectionHeader>عملاؤنا</SectionHeader>
-      <div>
+      <div className="flex flex-row">
+        <button className="button-prev">
+          <NavIcon src={prevIcon} />
+        </button>
+
         <Swiper
           modules={[Navigation]}
           spaceBetween={50}
           slidesPerView={3}
           loop={true}
-          navigation={true}
+          effect="cards"
+          grabCursor
+          navigation={{
+            prevEl: ".button-prev",
+            nextEl: ".button-next",
+          }}
+          preloadImages={false}
+          preventClicks={true}
+          slidesOffsetAfter={50}
+          breakpoints={{}}
         >
           <SwiperSlide>
-            <img src={ZidLogo} alt="" />
+            <div>
+              <img src={ZidLogo} alt="" />
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={MarsadLogo} alt="" />
+            <div>
+              <img src={MarsadLogo} alt="" />
+            </div>
           </SwiperSlide>
           <SwiperSlide>
-            <img src={MonshaatLogo} alt="" />
+            <div>
+              <img src={MonshaatLogo} alt="" />
+            </div>
           </SwiperSlide>
         </Swiper>
+        <button className="button-next">
+          <NavIcon src={nextIcon} />
+        </button>
       </div>
     </section>
   );

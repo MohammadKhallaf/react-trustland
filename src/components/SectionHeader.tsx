08d05f6637e778fce38380a2be51e9ddb5bc3eb5
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-08-31 19:29:12 +0200

    create section header component

--- src/components/SectionHeader.tsx
+++ src/components/SectionHeader.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const SectionHeader = ({ className, children }: Props) => {
   return (
-    <h3 className={`${className} font-bold text-3xl header__line`}>
+    <h3 className={`${className || ""} font-bold text-3xl header__line`.trim()}>
       {children}
     </h3>
   );

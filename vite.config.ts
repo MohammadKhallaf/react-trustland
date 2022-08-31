31b3701d64f1a7cc2043c0b00cc9a10d9591e9a8
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-08-31 19:27:33 +0200

    update configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import macrosPlugin from "vite-plugin-babel-macros";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), macrosPlugin()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
       "@img": path.resolve(__dirname, "src/assets/images"),
+      "@icon": path.resolve(__dirname, "src/assets/images/icons"),
     },
   },
 });

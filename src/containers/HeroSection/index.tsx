a426993534e66b72ed81189e94dad38f459cb907
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-09-01 00:23:15 +0200

    update hero section

--- src/containers/HeroSection/index.tsx
+++ src/containers/HeroSection/index.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-
+import SectionDiv from "../../components/SectionDiv";
 const HeroSection = (): JSX.Element => {
   return (
-    <section className="bg-black text-white h-screen grid place-items-center">
-      <div className="flex flex-col items-center gap-4">
+    <SectionDiv className="text-white h-full grid place-items-center" id="hero">
+      <div className="flex flex-col items-center gap-4 text-center">
         <h2 className="text-4xl font-bold">ترست لاين تقدم لأعمالك الحماية</h2>
         <p className="text-xs">
           ترست لاين شركة سعودية متخصصة في تقديم مجموعة واسعة من خدمات وحلول
           الأمن السيبراني.
         </p>
-        <button className="bg-mainBlue px-4 py-1 rounded-full mt-5">
+        <button className="text-md font-bold bg-mainBlue px-4 py-2 rounded-full mt-5">
           تواصل معنا
         </button>
       </div>
-    </section>
+    </SectionDiv>
   );
 };
 

a7cc01fe7e9906d4ce32ef205f392f9c7d096d83
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-09-01 00:24:40 +0200

    update team section

--- src/containers/TeamSection/index.tsx
+++ src/containers/TeamSection/index.tsx
@@ -1,23 +1,30 @@
 import React from "react";
 import SectionHeader from "../../components/SectionHeader";
-import "./TeamSection.css";
+import styles from "./TeamSection.module.css";
 
 const TeamSection = (): JSX.Element => {
   return (
-    <section className="py-5">
-      <div className="flex container-spacing">
-        <div className="teams__pic1 ml-3"></div>
-        <div className="teams__pic2 -ml-3"></div>
-        <div className="bg-lightGray my-auto rounded-2xl -z-[1] w-full pr-8 h-[250px] pl-[10%] flex flex-col justify-center">
+    <div className="flex container-spacing">
+      <div className={[styles.pic1, "hidden sm:block ml-3"].join(" ")}></div>
+      <div className={[styles.pic2, "hidden sm:block  -ml-3"].join(" ")}></div>
+      <div className="bg-lightGray my-auto rounded-2xl -z-[1] w-full pr-8 h-[250px] pl-[10%] flex flex-col justify-center">
+        <div className="flex flex-row items-center">
           <SectionHeader>فريقنا</SectionHeader>
-          <p>
-            في ترست لاين، نقدم للعملاء الحلول التي تجعلهم ينمون بثقة لسنوات
-            قادمة.
-          </p>
-          <p>يعمل موظفونا بشغف واستقلالية وإتقان لخلق عمل نفخر به جميعًا.</p>
+          <div className="mr-auto flex flex-row gap-3 items-center">
+            <div
+              className={[styles.pic1, styles.sm, "sm:hidden"].join(" ")}
+            ></div>
+            <div
+              className={[styles.pic2, styles.sm, "sm:hidden"].join(" ")}
+            ></div>
+          </div>
         </div>
+        <p>
+          في ترست لاين، نقدم للعملاء الحلول التي تجعلهم ينمون بثقة لسنوات قادمة.
+        </p>
+        <p>يعمل موظفونا بشغف واستقلالية وإتقان لخلق عمل نفخر به جميعًا.</p>
       </div>
-    </section>
+    </div>
   );
 };
 

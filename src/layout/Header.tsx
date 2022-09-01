c48513cdc77226cdd71141f62ef94de32389bb07
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-09-01 20:33:01 +0200

    update top navbar

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -21,27 +21,34 @@ const Header = (): JSX.Element => {
   return (
     <header className="h-screen hero-bg">
       {/* Nav bar */}
-      <div className="container-spacing py-3 md:pt-5 md:pb-3 flex flex-row  justify-between md:justify-around align-baseline gap-8 text-white lg:text-xl fixed z-10 shadow-md backdrop-blur-sm bg-zinc-900 bg-opacity-20 ">
-        <a href="" className="my-auto ">
-          <img src={logoSVG} alt="trust logo" className="h-8" />
-        </a>
-        <ul className="hidden md:flex flex-row flex-1 whitespace-nowrap justify-center items-center gap-5 ">
-          <NavListItem>ترست لاين</NavListItem>
-          <NavListItem>لماذا ترست لاين؟</NavListItem>
-          <NavListItem>آلية العمل</NavListItem>
-          <NavListItem>تواصل معنا</NavListItem>
-        </ul>
+      <div
+        className="
+      text-white lg:text-xl
+      fixed left-0 right-0 z-10
+      shadow-md backdrop-blur-sm bg-zinc-900 bg-opacity-20 "
+      >
+        <div className="container-spacing flex flex-row justify-between md:justify-around align-baseline  py-3 md:pt-5 md:pb-3  gap-8">
+          <a href="" className="my-auto ">
+            <img src={logoSVG} alt="trust logo" className="h-8" />
+          </a>
+          <ul className="hidden md:flex flex-row flex-1 whitespace-nowrap justify-center items-center gap-5 ">
+            <NavListItem>ترست لاين</NavListItem>
+            <NavListItem>لماذا ترست لاين؟</NavListItem>
+            <NavListItem>آلية العمل</NavListItem>
+            <NavListItem>تواصل معنا</NavListItem>
+          </ul>
 
-        <button className="hidden md:block rounded-full bg-white text-black px-3 py-1 text-sm">
-          دخول
-        </button>
-        <div className="hidden md:block">ENG</div>
-        <button
-          className="md:hidden nav_btn"
-          onClick={() => dispatch({ type: "show" })}
-        >
-          <MenuIcon className="w-8" />
-        </button>
+          <button className="hidden md:block rounded-full bg-white text-black px-3 py-1 text-sm">
+            دخول
+          </button>
+          <div className="hidden md:block">ENG</div>
+          <button
+            className="md:hidden nav_btn"
+            onClick={() => dispatch({ type: "show" })}
+          >
+            <MenuIcon className="w-8" />
+          </button>
+        </div>
       </div>
       <MobileNav show={state.show} close={() => dispatch({ type: "hide" })} />
       {/* Hero section */}

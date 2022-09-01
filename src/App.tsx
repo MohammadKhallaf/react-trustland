c24a982cfc30dd016ec7b57ae448a787bab3293c
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-09-01 23:56:17 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./layout";
 import Home from "./pages/Home";
+import PostDetails from "./pages/PostDetails";
+import Posts from "./pages/Posts";
 
 function App(): JSX.Element {
   return (
     <div className="App">
       <Layout>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="posts" element={<Posts />} />
+          <Route path="posts/:postId" element={<PostDetails />} />
+        </Routes>
       </Layout>
     </div>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+
+import App from "./App";
 
 import "./index.css";
 import "./swipper.css";
@@ -17,7 +18,9 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );

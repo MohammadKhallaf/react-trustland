0385775923ffe252b8eae34fd23f05f5e12b54f2
Mohammed Khallaf <mohamemd.khallaf.1997@gmail.com>
2022-09-01 20:21:10 +0200

    update news section

--- src/containers/NewsSection/NewsItem.tsx
+++ src/containers/NewsSection/NewsItem.tsx
@@ -5,13 +5,11 @@ interface ImgProps {
   url: string;
 }
 const NewsImgDiv = styled.div<ImgProps>`
-  aspect-ratio: 1 / 1;
   height: 100%;
-  width: 150px;
   max-width: 100%;
   background-image: url(${(props) => props.url});
   background-repeat: no-repeat;
-  background-size: auto 100%;
+
   background-position: 50%;
   background-color: rgba(26, 154, 251, 0.5);
   background-blend-mode: overlay;
@@ -26,9 +24,13 @@ interface Props {
 
 const NewsItem: React.FC<Props> = ({ img, title, description }) => {
   return (
-    <article className="flex flex-row bg-[#131414] p-5 rounded-xl hover:shadow-zinc-700 shadow-md transition-[box-shadow]">
-      <div className="basis-[150px] pl-5">
-        <NewsImgDiv url={img} className="rounded-xl shadow-inner" />
+    <article className="flex flex-col md:flex-row bg-[#131414] p-5 rounded-xl hover:shadow-zinc-700 shadow-md transition-[box-shadow]">
+      <div className="basis-[150px] pb-5 md:pb-0 md:pl-5">
+        <NewsImgDiv
+          url={img}
+          className="rounded-xl shadow-inner mx-auto w-full bg-cover
+          md:aspect-square md:w-[150px] md:bg-[size:auto_100%]"
+        />
       </div>
       <div className="flex flex-col justify-around max-w-[530px]">
         <h6 className="font-bold">{title}</h6>
